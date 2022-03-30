ae8411104910bc84e0155075eef24442100e2dfc
yogi4297 <71748169+yogi4297@users.noreply.github.com>
2022-03-29 20:49:16 -0500

    modified users.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -4,6 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'YogaNarasimhaReddy Peddireddy' });
+  res.render('index', { title: 'I have a Sports bike' });
+  
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var times=0
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  times=times+1
+  res.send(`Users accesses are: ${value}`);
+
 });
 
 module.exports = router;
